320103f5d7862680516ae3b0adfe7dccd4214e67
Vinicius Buchara <vbuchara31@gmail.com>
2024-09-22 17:47:42 -0400

    Finished some type conversion and operators learning

--- learn-csharp/Program.cs
+++ learn-csharp/Program.cs
@@ -1,9 +1,7 @@
-﻿
-using System.Runtime.InteropServices;
-
-namespace LearnCsharp {
+﻿namespace LearnCsharp
+{
     class Program {
-        static void Main(string[] args) {
+        public static void Main(string[] args) {
 
             Console.WriteLine("Select a method:");
             Console.WriteLine("1 - CheckedOverflow");
@@ -13,6 +11,7 @@ namespace LearnCsharp {
             Console.WriteLine("5 - VarVariable");
             Console.WriteLine("6 - StringPlaceholders");
             Console.WriteLine("7 - Conversions");
+            Console.WriteLine("8 - Operators");
 
             string? consoleInput = Console.ReadLine();
             
@@ -38,13 +37,16 @@ namespace LearnCsharp {
                 case "7":
                     Conversions();
                 break;
+                case "8":
+                    Operators();
+                break;
                 default:
                     Console.WriteLine("Invalid option.");
                 break;
             }
         }
 
-        static void CheckedOverflow(){
+        private static void CheckedOverflow(){
             Console.WriteLine("Enable Checked?");
             string? answerInput = Console.ReadLine();
 
@@ -64,7 +66,7 @@ namespace LearnCsharp {
             Console.WriteLine("Variable value: " + byteVariable);
         }
 
-        static void RealNumbers(){
+        private static void RealNumbers(){
             float floatNumber = 123.456f;
             double doubleNumber = 123.456;
             decimal decimalNumber = 123.456m;
@@ -74,7 +76,7 @@ namespace LearnCsharp {
             Console.WriteLine("Decimal: " + decimalNumber);
         }
 
-        static void TextVariables(){
+        private static void TextVariables(){
             // Quotes represent char values
             const char character = 'A';
             // Double quotes represent string values
@@ -84,13 +86,13 @@ namespace LearnCsharp {
             Console.WriteLine("Text: " + text);
         }
 
-        static void BooleanVariable(){
+        private static void BooleanVariable(){
             const bool isJoonPretty = true;
 
             Console.WriteLine("\nIs Joon Pretty? Yes! So " + isJoonPretty);
         }
 
-        static void VarVariable(){
+        private static void VarVariable(){
             var varStringVariable = "This is a anything variable";
             var varIntVariable = 32;
             var varDoubleVariable = 3.14159;
@@ -105,7 +107,7 @@ namespace LearnCsharp {
             Console.WriteLine("Type: " + varDoubleVariable.GetType().Name);
         }
 
-        static void StringPlaceholders(){
+        private static void StringPlaceholders(){
             string name = "Joon";
             int age = 25;
             string adjective = "hot";
@@ -113,13 +115,40 @@ namespace LearnCsharp {
             Console.WriteLine("\nBeautiful {0} is {1} and {2}!!", name, age, adjective);
         }
 
-        static void Conversions(){
+        private static void Conversions(){
             // Implicit conversion
             int intNumber = 123;
             float floatNumber = intNumber;
 
             Console.WriteLine($"\nIntNumber: {intNumber} of type {intNumber.GetType().Name}");
             Console.WriteLine($"FloatNumber: {floatNumber} of type {floatNumber.GetType().Name}");
+
+            // Explicit conversion
+            double doubleNumber = 123.0;
+            int anotherIntNumber = (int) doubleNumber;
+
+            Console.WriteLine($"\nDoubleNumber: {doubleNumber} of type {doubleNumber.GetType().Name}");
+            Console.WriteLine($"IntegerNumber: {anotherIntNumber} of type {anotherIntNumber.GetType().Name}");
+
+            // Non-compatible conversion
+            string stringNumber = "123";
+
+            int firstConversion = Convert.ToInt32(stringNumber);
+            int secondConversion = int.Parse(stringNumber);
+
+            Console.WriteLine($"\nStringNumber: {stringNumber} of type {stringNumber.GetType().Name}");
+            Console.WriteLine($"FirstConversion: {firstConversion} of type {firstConversion.GetType().Name}");
+            Console.WriteLine($"SecondConversion: {secondConversion} of type {secondConversion.GetType().Name}");
+        }
+
+        private static void Operators(){
+            int firstVariable = 10;
+            // Assigns the current values of firstVariable (10) to secondVariable and
+            // then increment firstVariable by one
+            int secondVariable = firstVariable++;
+
+            Console.WriteLine($"\nFirst Variable: {firstVariable}");
+            Console.WriteLine($"Second Variable: {secondVariable}");
         }
     }
 
